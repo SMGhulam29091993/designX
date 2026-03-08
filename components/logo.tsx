0b7961f46ec5dfa9d9520a928c7e33493e24c278
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-03-09 02:54:14 +0530

    feat: added logo component

---
+++ components/logo.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <>
+      <Link href="/" className="flex-1 flex items-center gap-2 text-2xl cursor-pointer">
+        <span className="inline-block font-extrabold text-primary">Design</span>
+        <span className="font-semibold text-foreground">X.ai</span>
+      </Link>
+    </>
+  );
+};
+
+export default Logo;
