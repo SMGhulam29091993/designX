d6c94c40c700e940535a1b1b0d9e5e611722cd39
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-03-08 16:23:07 +0530

    feat: initial commit

---
+++ app/(route)/layout.tsx
@@ -0,0 +1,9 @@
+export default function AppLayout({
+    children,
+    }: Readonly<{
+    children: React.ReactNode;
+    }>) {
+    return (
+        <main className="w-full h-auto">{children}</main>
+    )
+}
